d2763cc33f1f5379513ef4120d7ac3c45c45db49
Igor Aherne <igor.aherne.videos@gmail.com>
2025-01-05 01:20:15 +0200

    removed 2 includes (duplicates)

--- api_spz/main_api.py
+++ api_spz/main_api.py
@@ -8,8 +8,6 @@ import torch
 
 # # only used for debugging, to emulate low-vram graphics cards:
 #
-import torch
-import os
 #torch.cuda.set_per_process_memory_fraction(0.43)  # Limit to 43% of my available VRAM, for testing.
 # And/or set maximum split size (in MB)
 #os.environ['PYTORCH_CUDA_ALLOC_CONF'] = 'max_split_size_mb:128,garbage_collection_threshold:0.8'
