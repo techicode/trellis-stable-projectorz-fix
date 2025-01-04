0ed9ffe11647ff52dceeb71ee1d0aff6a07f2bc9
Igor Aherne <igor.aherne.videos@gmail.com>
2025-01-04 14:29:40 +0200

    loggers don't print long date-time in messages

--- api_spz/main.py
+++ api_spz/main.py
@@ -19,11 +19,6 @@ print(
 # os.environ['PYTORCH_CUDA_ALLOC_CONF'] = 'max_split_size_mb:128,garbage_collection_threshold:0.8'
 
 import logging
-logger = logging.getLogger("trellis")
-logger.setLevel(logging.INFO)
-logger.propagate = False  # Prevent messages from propagating to root
-handler = logging.StreamHandler()
-handler.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s',  datefmt='%H:%M:%S'))
 
 class TritonFilter(logging.Filter):# Custom filter to ignore Triton messages
     def filter(self, record):
@@ -34,10 +29,22 @@ class TritonFilter(logging.Filter):# Custom filter to ignore Triton messages
             "no module named 'triton'"
         ]
         return not any(phrase in message for phrase in triton_phrases)
+    
+logger = logging.getLogger("trellis")
+logger.setLevel(logging.INFO)
+logger.propagate = False  # Prevent messages from propagating to root
+handler = logging.StreamHandler()
+handler.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s',  datefmt='%H:%M:%S'))
+#and to our own "trellis" logger:
+handler.addFilter(TritonFilter())
+logger.addHandler(handler)
+# other scripts can now use this logger by doing 'logger = logging.getLogger("trellis")'
+
 # Configure root logger:
 logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s - %(levelname)s - %(message)s',
+    datefmt='%H:%M:%S', # Only show time
     handlers=[logging.StreamHandler()]
 )
 # Apply TritonFilter to all root handlers
@@ -45,10 +52,7 @@ root_logger = logging.getLogger()
 for handler in root_logger.handlers:
     handler.addFilter(TritonFilter())
 
-#and to our own "trellis" logger:
-handler.addFilter(TritonFilter())
-logger.addHandler(handler)
-# other scripts can now use this logger by doing 'logger = logging.getLogger("trellis")'
+
 
 
 # -------------- PIPELINE SETUP ----------------
